eff1e989ad7dc4ac102796f552cf56fb724378d9
Alex Luo <theaxluo@gmail.com>
2022-06-24 23:58:59 -0500

    new menu

--- Scripts/UI/MenuScript.cs
+++ Scripts/UI/MenuScript.cs
@@ -5,25 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Canvas menuCanvas;
+    [SerializeField] private Canvas settingsCanvas;
 
     public void StartGame()
     {
         SceneManager.LoadScene("Level_1");
+        menuCanvas.enabled = true;
     }
 
     public void ShowCredits()
     {
         SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        settingsCanvas.enabled = true;
+        menuCanvas.enabled = false;
     }
 }
