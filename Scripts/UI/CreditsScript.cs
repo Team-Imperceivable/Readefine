3a47dd39e374c10b4e495e3ad88553f5e1c409e1
Alex Luo <theaxluo@gmail.com>
2022-06-28 09:20:36 -0500

    Redid credits

--- Scripts/UI/CreditsScript.cs
+++ Scripts/UI/CreditsScript.cs
@@ -11,40 +11,22 @@ public class CreditsScript : MonoBehaviour
     [SerializeField] private Canvas parentCanvas;
     [SerializeField] private float endY;
     private RectTransform[] zones;
-    private string storedWord = "Thanks For Playing!";
-    
 
     // Start is called before the first frame update
     void Start()
     {
-        zones = GetComponentsInChildren<RectTransform>();
         delayTime += Time.time;
     }
 
     void Update()
     {
-        if(Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2"))
-        {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            foreach(RectTransform zone in zones)
-            {
-                if(zone.rect.Contains(mousePos))
-                {
-                    Text textBox = zone.gameObject.GetComponent<Text>();
-                    string word = textBox.text;
-                    textBox.text = storedWord;
-                    storedWord = word;
-                }
-            }
-        }
-
         if(Time.time > delayTime)
         {
             Vector3 change = new Vector3(0f, scrollSpeed, 0f);
             transform.position += change;
         }
 
-        if(transform.position.y > endY)
+        if(transform.position.y > endY || Input.GetButtonDown("Cancel"))
         {
             SceneManager.LoadSceneAsync("Menu");
         }
